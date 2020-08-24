d8a11f92090b62cbdda0a0d14d9a1cbd0ef35261
unknown <hack-beep@nandinisharma26jan2000@gmail.com>
2020-08-24 19:41:15 +0530

    next commit

---
+++ script.js
@@ -0,0 +1,16 @@
+<script src="script.js">
+      var MenuItems = document.getElementById("MenuItems");
+      MenuItems.style.max-Height="0px";
+
+      function menutoggle(){
+        if( MenuItems.style.max-Height=="0px"){
+
+           MenuItems.style.max-Height="200px"
+        }
+
+        else{
+           MenuItems.style.max-Height="0px"
+        }
+      }
+
+    </script>
